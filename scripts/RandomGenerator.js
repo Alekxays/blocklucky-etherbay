db6bb1657c91151024e1ed48692e1fca71613100
JeuneKaiser <enzobiger@gmail.com>
2025-11-13 11:57:32 +0100

    patch

--- scripts/RandomGenerator.js
+++ scripts/RandomGenerator.js
@@ -1,20 +1,8 @@
-const { ethers } = require("ethers") ;
-import { crypto } from "crypto";
-
-/** 
-const randomUint256 = ethers.toBigInt(
-    "0x" + [...crypto.getRandomValues(new Uint8Array(32))]
-    .map(b => b.toString(16).padStart(2, '0'))
-    .join('')
-);
-
-console.log(randomUint256)
-*/
-
+import { ethers } from "ethers";
+import crypto from "crypto";
 
 
 const randomBytes = crypto.randomBytes(32);
+const randomUint256 = BigInt("0x" + randomBytes.toString("hex"));
 
-const randomUint256 = ethers.toBigInt("0x" + randomBytes.toString("hex"));
-
-console.log("Random number:" , randomUint256);
+console.log("Random number:", randomUint256);
